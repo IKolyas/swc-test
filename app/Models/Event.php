220e746c717e7920aad862d93d9b05dbfa1741f2
IKolyas <i.kolyasnikov@mail.ru>
2023-10-21 18:03:37 +0500

    1: add seed

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Event extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['title', 'description', 'user_id'];
 
     public $casts = ['created_at' => 'date'];
